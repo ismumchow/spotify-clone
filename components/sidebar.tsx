8bb1a3b286cc4dbabd1d6e76fe5eb03896a308c7
ismum <ichowdhury4@fordham.edu>
2023-01-26 19:47:36 -0500

    created playerlayout and sidebar component

---
+++ components/sidebar.tsx
@@ -0,0 +1,34 @@
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from '@chakra-ui/layout'
+
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from 'react-icons/md'
+
+const Sidebar = () => {
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="grey"
+    >
+      shabalabadingdong
+    </Box>
+  )
+}
+
+export default Sidebar
