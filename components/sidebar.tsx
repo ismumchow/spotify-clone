6bc6f4a326bc5b539d85d19a61bdd022ff376022
ismum <ichowdhury4@fordham.edu>
2023-01-27 10:41:40 -0500

    added menu items to the sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,5 @@
 import NextImage from 'next/image'
+import NextLink from 'next/link'
 import {
   Box,
   List,
@@ -18,6 +19,24 @@ import {
   MdFavorite,
 } from 'react-icons/md'
 
+const navMenu = [
+  {
+    name: 'Home',
+    icon: MdHome,
+    route: '/',
+  },
+  {
+    name: 'Search',
+    icon: MdSearch,
+    route: '/search',
+  },
+  {
+    name: 'Your Library',
+    icon: MdLibraryMusic,
+    route: '/library',
+  },
+]
+
 const Sidebar = () => {
   return (
     <Box
@@ -30,6 +49,26 @@ const Sidebar = () => {
       <Box paddingY="20px">
         <Box width="120px" marginBottom="20px" paddingX="20px">
           <NextImage src="/logo.svg" height={60} width={120} />
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {navMenu.map((menuItem) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menuItem.name}>
+                <LinkBox>
+                  <NextLink href={menuItem.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menuItem.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menuItem.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
         </Box>
       </Box>
     </Box>
